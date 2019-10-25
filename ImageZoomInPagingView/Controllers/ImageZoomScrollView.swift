7d1bb2cd00133bb72c3f58429c64025bb0e48699
Harut Mikichyan <harutmikichyan@Haruts-Mac-mini.local>
2019-10-25 11:47:59 +0400

    Initial Commit

---
+++ ImageZoomInPagingView/Controllers/ImageZoomScrollView.swift
@@ -0,0 +1,71 @@
+//
+//  ImageZoomScrollView.swift
+//  ImageZoomInPagingView
+//
+//  Created by Harut Mikichyan on 10/25/19.
+//  Copyright © 2019 Harut Mikichyan. All rights reserved.
+//
+
+import UIKit
+
+class ImageZoomScrollView: UIScrollView {
+
+    var imageView: UIImageView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        delegate = self
+        minimumZoomScale = 0.05
+        maximumZoomScale = 2
+        imageView = UIImageView(image: UIImage(named: "image.jpg"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        addSubview(imageView)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(tap:)))
+        tap.numberOfTapsRequired = 2
+        addGestureRecognizer(tap)
+    }
+    
+    @objc func handleDoubleTap(tap:UITapGestureRecognizer){
+        if zoomScale > minimumZoomScale {
+            setZoomScale(minimumZoomScale, animated: true)
+        } else {
+            let location = tap.location(in: imageView)
+            let rect = CGRect(x: location.x, y: location.y, width: 1, height: 1)
+            zoom(to: rect, animated: true)
+        }
+    }
+
+    private func centerContent() {
+        let imageViewSize = imageView.frame.size
+
+        var vertical:CGFloat = 0, horizontal:CGFloat = 0
+        if imageViewSize.width < bounds.size.width  {
+            vertical = (bounds.size.width - imageViewSize.width) / 2.0
+        }
+
+        if imageViewSize.height < bounds.size.height  {
+            horizontal = (bounds.size.height - imageViewSize.height) / 2.0
+        }
+
+        contentInset = UIEdgeInsets(top: horizontal, left: vertical, bottom: horizontal, right: vertical)
+    }
+}
+
+extension ImageZoomScrollView: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerContent()
+    }
+}
+
---
+++ ImageZoomInPagingView/Controllers/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  ImageZoomInPagingView
+//
+//  Created by Harut Mikichyan on 10/25/19.
+//  Copyright © 2019 Harut Mikichyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var scrollView: UIScrollView!
+    var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScrollView()
+        setupPageControl()
+        
+        var x = 0
+        let y = 0
+        let width = self.scrollView.frame.size.width
+        let height = self.scrollView.frame.size.height
+        for _ in 1...10 {
+            let imageScrollView = ImageZoomScrollView(frame: CGRect(x: CGFloat(x), y: CGFloat(y), width: width, height: height))
+            scrollView.addSubview(imageScrollView)
+            x = x + Int(width)
+        }
+    }
+    
+    @objc func changePage(sender:UIPageControl) {
+        let offsetX = CGFloat(sender.currentPage) * CGFloat(scrollView.frame.size.width)
+        scrollView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
+    }
+    
+    //MARK: - Private Interface
+    private func setupScrollView() {
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height - 50.0))
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        scrollView.minimumZoomScale = 0.05
+        scrollView.maximumZoomScale = 2
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.contentSize = CGSize(width: 10.0*scrollView.bounds.size.width, height: 0)
+        self.view.addSubview(scrollView)
+    }
+    
+    private func setupPageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: view.bounds.size.height - CGFloat(50), width: self.view.bounds.size.width, height: 50.0))
+        pageControl.numberOfPages = 10
+        pageControl.addTarget(self, action: #selector(changePage(sender:)), for: .valueChanged)
+        pageControl.pageIndicatorTintColor = UIColor.red
+        pageControl.currentPageIndicatorTintColor = UIColor.blue
+        self.view.addSubview(pageControl)
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / self.scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+    }
+}
+
+
